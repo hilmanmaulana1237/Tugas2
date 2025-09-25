9081d69d46a66f00b2d513dcac3c7fa2126314b3
hilmanmaulana1237 <1237050020@student.uinsgd.ac.id>
2025-09-25 21:10:06 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas2/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.tugas2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var mCount = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // restore state jika rotate
+        mCount = savedInstanceState?.getInt("count", 0) ?: 0
+
+        val mShowCount = findViewById<TextView>(R.id.show_count)
+        val buttonCountUp = findViewById<Button>(R.id.button_count)
+        val buttonToast = findViewById<Button>(R.id.button_toast)
+
+        // tampilkan nilai awal
+        mShowCount.text = mCount.toString()
+
+        buttonCountUp.setOnClickListener {
+            mCount++
+            mShowCount.text = mCount.toString()
+        }
+
+        buttonToast.setOnClickListener {
+            val tulisan = mShowCount.text.toString()
+            Toast.makeText(this, "Angka yang dimunculkan $tulisan", Toast.LENGTH_LONG).show()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val bars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(bars.left, bars.top, bars.right, bars.bottom)
+            insets
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("count", mCount)
+        super.onSaveInstanceState(outState)
+    }
+}
